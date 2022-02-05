4c436882f938824ef086104fe023335a9ac8379d
Alex Titarenko <alx.titarenko@gmail.com>
2022-02-04 21:22:30 -0800

    update messenger app

--- apps/facebook-messenger.sh
+++ apps/facebook-messenger.sh
@@ -1,7 +1,9 @@
 name="Messenger"
 icon="facebook-messenger.icns"
-url="https://www.facebook.com/messages"
+url="https://messenger.com"
 
 npx nativefier "$url" \
     --name "$name" \
-    --icon "icons/$icon"
+    --icon "icons/$icon" \
+    --single-instance \
+	--internal-urls "www.messenger.com"
