54f7b907e6c65713153f1e121b290c19c2bb375f
Alex Titarenko <alx.titarenko@gmail.com>
2022-02-04 16:33:10 -0800

    add multicalc and noteshub apps

---
+++ multicalc.sh
@@ -0,0 +1,7 @@
+name="MultiCalc"
+icon="multicalc.icns"
+url="https://www.multicalculator.app"
+
+npx nativefier "$url" \
+    --name "$name" \
+    --icon "icons/$icon"
---
+++ noteshub.sh
@@ -0,0 +1,7 @@
+name="NotesHub"
+icon="noteshub.icns"
+url="https://www.noteshub.app"
+
+npx nativefier "$url" \
+    --name "$name" \
+    --icon "icons/$icon"
